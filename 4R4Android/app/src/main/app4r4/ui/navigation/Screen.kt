c8eaf1d873b2547352a20abc0b13ad278b33a0e0
João Pedro Aranha Moscatello <joaoaranham@gmail.com>
2026-03-14 19:40:45 -0300

    Delete 4R4Android/app/src/main/app4r4 directory

--- 4R4Android/app/src/main/app4r4/ui/navigation/Screen.kt
+++
@@ -1,9 +0,0 @@
-package com.app4r4.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Onboarding : Screen("onboarding")
-    object Dashboard : Screen("dashboard")
-    object Calculator : Screen("calculator")
-    object Tips : Screen("tips")
-    object History : Screen("history")
-}
